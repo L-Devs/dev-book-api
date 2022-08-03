1cb6f29501df02231711eedbdf89f584709029c5
Osama <Udokakai@gmail.com>
2022-08-03 18:44:58 +0200

    Finished stage 1 of the login route

--- dev_book_api/LAuth/views.py
+++ dev_book_api/LAuth/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+def login(request):
+    email = request.GET.get('email')
+    password = request.GET.get('password')
+    return HttpResponse('signed in with:'+email)
