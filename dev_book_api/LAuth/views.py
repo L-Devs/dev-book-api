b762862debab1fe31f48292b388aefcabf23d736
Osama <Udokakai@gmail.com>
2022-08-07 00:15:01 +0200

    minor bug fixes
    changed error response for missing email/password.

--- dev_book_api/LAuth/views.py
+++ dev_book_api/LAuth/views.py
@@ -1,9 +1,14 @@
-from django.http import HttpResponse
+from lib2to3.pgen2.parse import ParseError
+from types import NoneType
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
+
 import requests
 
 # Create your views here.
 def login(request):
     email = request.GET.get('email')
     password = request.GET.get('password')
+    if (email == None or password == None):
+        return HttpResponseBadRequest('No email or password.')
     return HttpResponse('signed in with:'+email)
