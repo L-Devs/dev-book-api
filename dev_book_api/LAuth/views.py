8fe86de7ee6d90c3d14a99e9d0fbafa044f0c336
Abubakre <bakry.osama9@gmail.com>
2022-08-28 18:53:48 +0200

    create token on successful login
    create a jwt on login and save it in the UserAuth table and return in in JSON

--- dev_book_api/LAuth/views.py
+++ dev_book_api/LAuth/views.py
@@ -1,22 +1,40 @@
 from datetime import datetime
+import email
 import json
 from lib2to3.pgen2 import token
-from django.http import HttpResponse, HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 import requests
 from .models import UserAuth, User
 from datetime import datetime, timedelta
-
+import jwt
+import json
 
 # Create your views here.
 def login(request):
     if (request.method == "POST"):
         requestBodyUnicode = request.body.decode('utf-8')
         requestBody = json.loads(requestBodyUnicode)
-        email = requestBody['email']
-        password = requestBody['password']
+        
 
-        if (email == None or password == None):
-            return HttpResponseBadRequest('No email or password.')   
+        if (requestBody['email'] == None or requestBody['password'] == None):
+            return HttpResponseBadRequest('No email or password.')
+        secret_key = "6e010f75d1c0245a8631cb13371cd9dafcdaa5b3"
+        #try:
+        uservalues = User.objects.filter(email=requestBody['email']).values()
+        userauthvalues = UserAuth.objects.filter(userid=uservalues[0]['userid']).values()
+        if (uservalues[0]['email'] == requestBody['email'] and userauthvalues[0]['password']):
+            userauthobject = UserAuth.objects.get(username=uservalues[0]['username'])
+            userauthobject.token = jwt.encode(payload=requestBody, key=secret_key)
+            userauthobject.token_expiration = datetime.now() + timedelta(days = 2)
+            userauthobject.save()
+            tokendict = {"token" : userauthobject.token}
+            tokenjson = json.dumps(tokendict)
+            return JsonResponse(tokenjson, safe=False)
+        #except:
+         #   return HttpResponseBadRequest("Email isn't in database")
+
+
+        
 
     else:
         return HttpResponseBadRequest("not a post request")
@@ -36,12 +54,11 @@ def signup(request):
         email = requestBody['email']
         password = requestBody['password']
         username = requestBody['username']
-        token = requestBody['token']
-        token_expiration = datetime.now() + timedelta(days = 2)
+
         if (email == None or password == None or username == None):
             return HttpResponseBadRequest("Signup Failed")
         else:
-            userauthz = UserAuth(username=username, password=password, token=token, token_expiration=token_expiration)
+            userauthz = UserAuth(username=username, password=password)
             userauthz.save()
 
             temp = UserAuth.objects.filter(username=username).values()
