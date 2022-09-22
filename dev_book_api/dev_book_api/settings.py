6e7caec3865e83e8458aaf50b8306f6eb76c1efa
Ahmed Hamad <60454980+ahmedhamad-okkio@users.noreply.github.com>
2022-09-22 20:55:46 +0100

    Update settings.py

--- dev_book_api/dev_book_api/settings.py
+++ dev_book_api/dev_book_api/settings.py
@@ -93,6 +93,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
   'http://localhost:8000',
